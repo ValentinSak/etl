92a37d6b84693c72add878468125d85b606626ef
valentinsak <valentinsak@gmail.com>
2025-04-12 21:07:01 +0300

    fixed minor issues

--- etl_db/procedures/create_schema.sql
+++ etl_db/procedures/create_schema.sql
@@ -39,17 +39,22 @@ CREATE TABLE etl.sales (
 
 CREATE TABLE etl.raw_events (
     id SERIAL PRIMARY KEY,
-    batch_id TEXT;
+    batch_id TEXT,
     event_type TEXT,
     payload JSONB,
     created_at TIMESTAMP DEFAULT now()
 );
 
+COMMIT;
+
+BEGIN;
+
 CREATE INDEX idx_sales_order_id ON etl.sales(order_id);
 CREATE INDEX idx_sales_store_number ON etl.sales(store_id);
 CREATE INDEX idx_sales_product_id ON etl.sales(product_id);
-CREATE INDEX idx_sales_sale_date ON etl.sales(sale_date);
-CREATE INDEX idx_raw_events_event_type ON raw_events(event_type);
-CREATE INDEX idx_raw_events_created_at ON raw_events(created_at);
+CREATE INDEX idx_sales_sale_date ON etl.sales(sale_date);   
+CREATE INDEX idx_raw_events_event_type ON etl.raw_events(event_type);
+CREATE INDEX idx_raw_events_created_at ON etl.raw_events(created_at);
+
 
 COMMIT;
