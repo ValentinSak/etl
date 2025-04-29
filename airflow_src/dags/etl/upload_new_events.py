7fb3a8688ad728d201d66110bf71c8946d96b3a2
valentinsak <valentinsak@gmail.com>
2025-04-29 22:51:31 +0300

    minor changes

--- airflow_src/dags/etl/upload_new_events.py
+++ airflow_src/dags/etl/upload_new_events.py
@@ -7,7 +7,7 @@ from lib.service import upload_events, get_all_processed_files
 from lib.configs import configs
 
 
-dag_name = os.path.basename(__file__).replace('.py', '')
+dag_name = os.path.basename(__file__).replace(".py", "")
 default_args = {
     'start_date': datetime(2024, 1, 1),
 }
@@ -20,34 +20,32 @@ shared_dir_path = configs['shared_dir_path']
 
 
 def upload_events_wrapper(shared_dir_path: str) -> None:
-    """Wrapper function for uploading events to the database
-    
+    '''Wrapper function for uploading events to the database
+
     params:
     shared_dir_path - path to the directory containing event files
-    
+
     The function:
     1. Gets list of processed files
     2. Uploads events from unprocessed files to the database
-    """
+    '''
     processed_files = get_all_processed_files()
     upload_events(shared_dir_path, processed_files)
 
 
 with DAG(
-        dag_name,
-        default_args=default_args,
-        description='write events to csv file in shared directory',
-        start_date=datetime(2024, 1, 1),
-        schedule_interval='10 * * * *',
-        catchup=False,
-        max_active_runs=1,
-        tags=[
-            'fill_tables'
-        ]
+    dag_name,
+    default_args=default_args,
+    description='write events to csv file in shared directory',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='10 * * * *',
+    catchup=False,
+    max_active_runs=1,
+    tags=['fill_tables'],
 ) as dag:
     PythonOperator(
         task_id=upload_events.__name__,
         python_callable=upload_events_wrapper,
         op_args=[shared_dir_path],
-        dag=dag
+        dag=dag,
     )
